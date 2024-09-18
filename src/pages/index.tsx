b511a4631b1decb554f4e53d46ae2b2ef0c9505f
andreamaurino <andrea.maurino@unimib.it>
2024-09-18 15:06:06 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,21 +17,21 @@ import {
 
 const features = [
   {
-    name: "Friendly UI system for interacting and managing STI approaches",
+    name: "Friendly library for contaminating existing dataset",
     description:
-      "We know designing a user-friendly, robust UI can be a challenge and can demand both technical prowess and design skills, that is why we developed MantisTable UI",
+      "We know designing and implementing a data contamination systems can be a challenge and can demand both technical prowess and design skills, that is why we developed Pucktrick",
     icon: SquareChartGantt,
   },
   {
-    name: "Visualisation of annotations",
+    name: "Controlled contamination level",
     description:
-      "With MantisTable UI, you can easily visualise table annotations, providing a clear and intuitive overview of your data",
+      "With Pucktrick, you can easily define the level of contamination that you want to inject into dataset",
     icon: SquarePen,
   },
   {
-    name: "Powerful and flexible plugin system",
+    name: "Wide range of real data error",
     description:
-      "This incredible feature enables users to easily extend and customise its functionality, offering a seamless experience and allowing for effortless expansion and personalisation",
+      "with Pucktrick you will be able to simulate real error in your data, in order to recreate the real operating conditions of the datasets. ",
     icon: Blocks,
   },
 ];
@@ -41,33 +41,32 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="MantisTable UI was designed to let you tame your Semantic Table Interpretation approach."
+      description="Pucktrick was designed to let you work with controlled contaminated datasets"
     >
       <main>
         <img
-          src="/mantistable-ui-docs/img/bblurry.svg"
+          src="/pucktrick-docs/img/bblurry.svg"
           className="absolute opacity-15 -top-[45rem]"
         />
         <section className="relative z-10">
           <div className="place-content-center mx-4 lg:gap-12 xl:gap-12 lg:pt-10 lg:grid-cols-12 pb-12">
             <h1 className="md:text-center pt-12 mb-4 leading-none text-gray-900 sm:text-6xl md:text-7xl lg:text-8xl dark:text-white font-bold tracking-widest">
-              MantisTable UI
+              Pucktrick
             </h1>
             <div className="justify-center flex flex-col md:flex-row max-w-6xl gap-20 pb-2 mx-auto">
               <img
                 alt="Logo"
-                src="/mantistable-ui-docs/img/mantislogo.svg"
+                src="/Pucktrick-docs/img/mantislogo.svg"
                 className="lg:mt-0 lg:col-span-4 lg:flex w-[20rem] object-fit-cover h-auto max-w-full"
               />
               <div className="place-content-center max-w-lg pl-2">
                 <p className=" text-dgBackground dark:text-white sm:text-xl lg:mb-8 md:text-2xl lg:text-3xl">
                   {" "}
-                  Effortless data interaction and simple table navigation with a
-                  Graphic User Interface for Semantic Table Interpretion{" "}
+                  Effortless data contamination for data centric AI studies{" "}
                 </p>
                 <div className="flex flex-row">
                   <a
-                    href="/mantistable-ui-docs/docs/introduction"
+                    href="/pucktrick-docs/docs/introduction"
                     className="text-center inline-flex items-center border-none dark:bg-primaryGreen dark:text-lightBackground  hover:text-dgBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-secondaryGreen font-semibold"
                   >
                     Learn more <ArrowRight className="ml-2 h-5 w-5" />{" "}
@@ -78,12 +77,7 @@ export default function Home(): JSX.Element {
                   >
                     Feedback and Features request <ExternalLink className="ml-2 h-5 w-5" />{" "}
                   </a>
-                  <a
-                    href="https://mantistable.datai.disco.unimib.it/"
-                    className="text-center inline-flex items-center border-2 border-solid dark:border-primaryGreen border-secondaryGreen hover:text-dgBackground dark:text-lightBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 font-semibold"
-                  >
-                    Try it out <ExternalLink className="ml-2 h-5 w-5" />{" "}
-                  </a>
+                 
                 </div>
               </div>
             </div>
@@ -96,13 +90,11 @@ export default function Home(): JSX.Element {
               <div className="flex flex-col justify-center lg:pr-8 lg:pt-4">
                 <div className="lg:max-w-lg">
                   <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-                    An intuitive UI for Semantic Table Interpretation
+                    An intuitive library for data contamination
                   </h2>
                   <p className="mt-4 text-lg leading-6 text-gray-600 dark:text-gray-300">
-                    MantisTable UI is the ultimate web interface for managing
-                    Semantic Table Interpretation (STI) approaches, providing an
-                    intuitive, ready-to-use platform tailored anyone who is
-                    interested in STI world.
+                    Pucktrick is the ultimate library for managing introducing a controlled amount of specific errors into datasets
+                    trohugh  providing  intuitive methods for simulating real operating environment.
                   </p>
                   <div className="mt-6 max-w-xl space-y-4 text-base leading-5 text-gray-600 lg:max-w-none">
                     {features.map((feature) => (
@@ -124,7 +116,7 @@ export default function Home(): JSX.Element {
               </div>
               <img
                 alt="Product screenshot"
-                src="/mantistable-ui-docs/img/mantisUI.png"
+                src="/pucktrick-ui-docs/img/mantisUI.png"
                 className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
               />
             </div>
