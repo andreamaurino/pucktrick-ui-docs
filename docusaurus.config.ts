85d3eaef568499e39ede5b03dd0ce693c39ae501
andreamaurino <andrea.maurino@unimib.it>
2024-09-18 14:19:17 +0200

    version 0.1

---
+++ docusaurus.config.ts
@@ -0,0 +1,104 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-plugin.cjs';
+
+const config: Config = {
+  title: 'Pucktrick',
+  tagline: 'Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://andreamaurino.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/pucktrick-ui-docs/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Andrea Maurino', // Usually your GitHub org/user name.
+  projectName: 'Pucktrick', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '1.0.0',
+              badge :true,
+            },
+          },
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/mantistable-ui-social-card.png',
+    navbar: {
+      title: 'MantisTable UI',
+      logo: {
+        alt: 'MantisTable UI Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://andreamaurino.github.io/sti-website/',
+          label: 'Our works',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/andreamaurino/pucktrick',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ]
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Dat<sup>*</sup>AI lab lab. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    colorMode: {
+      defaultMode: 'dark'
+    },
+  } satisfies Preset.ThemeConfig,
+
+  // Tailwind
+  plugins: [tailwindPlugin],
+};
+
+export default config;
